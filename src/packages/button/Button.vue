6c559e549896bec1609ed1a5ca706a865cefe2c5
leigang <1844711699@qq.com>
2020-10-22 18:16:42 +0800

    修改

--- src/packages/button/Button.vue
+++ src/packages/button/Button.vue
@@ -1,16 +1,81 @@
 <template>
-    <div style="border: 1px red solid; height: 60px; width: 160px;"></div>
+    <div >
+        <button>
+             <span v-if="this.$slots.default">
+                <slot></slot>
+             </span>
+        </button>
+
+        <div id="time-shaft">
+            <div v-for="(item,index) in dataShaft">
+                <span>{{ item }}</span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    const test = '';
     export default {
         name: "wt-button",
+        props:{
+            dateList:{
+                type:Object,
+                default:()=>({
+                    startDate:'2019-05-01',
+                    endDate:'2029-10-22',
+                })
+            },
+        },
+        watch:{
+            dateList:{
+                deep:true,
+                immediate:true,
+                handler(val){
+                    let startYear = new Date(this.dateList.startDate).getFullYear();
+                    let endDate = new Date(this.dateList.endDate).getFullYear();
+                    let dateSpace = endDate - startYear;
+                    /*结束年份大于开始年份*/
+                    if(dateSpace > 0){
+                        for(let i = 0; i <= dateSpace; i++){
+                            let temp = null;
+                            if(i == 0){
+                                temp = this.flagMonth.slice(new Date(this.dateList.startDate).getMonth());
+                            }else if(i == dateSpace){
+                                temp = this.flagMonth.slice(0,new Date(this.dateList.endDate).getMonth()+1);
+                            }else{
+                                temp = this.flagMonth;
+                            }
+                            this.dataShaft.push(
+                                startYear+i,
+                                ...temp
+                            );
+                        }
+                    }
+                    if(dateSpace == 0){
+                        /*同年份 - 闭区间 所以加一*/
+                        this.dataShaft.push(startYear,...this.flagMonth.slice(new Date(this.dateList.startDate).getMonth(),new Date(this.dateList.endDate).getMonth() + 1));
+                    }
+                }
+            }
+        },
+        data(){
+            return{
+                dataShaft:[],
+                flagMonth:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+            }
+        },
         created(){
             console.log('slots',this.$slots);
         }
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+#time-shaft{
+    border: 1px red solid;
+    width: 100px;
+    min-height: 600px;
+    background-color: #eee;
+}
 </style>
--- vue-press/docs/components/button.md
+++ vue-press/docs/components/button.md
@@ -3,15 +3,10 @@
 ## 基础用法
 基础的按钮用法。
 
-# 按钮
+## 按钮
 <template>
     <wt-button></wt-button>
 </template>
 
 
-使用type属性来定义 Button 的样式。
-```html
-<div>
-    <wt-button></wt-button>
-</div>
-```
+
